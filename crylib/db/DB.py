2155373fca12e10bdf0882c058328ccb45570b5f
oalieno <jeffrey6910@gmail.com>
2019-10-14 15:22:20 +0800

    add crylib db

--- crylib/db/DB.py
+++ crylib/db/DB.py
@@ -1,11 +1,13 @@
 from collections import defaultdict
+from .. import Constant
 
 class DB:
     def __init__(self, description):
         self.description = description
-        self.collections = defaultdict(list)
-    def add(self, algo, collection):
-        self.collections[algo].append(collection)
+        self.constants = defaultdict(list)
     def load(self, data):
         for item in data:
-            self.add(item['algo'], item['collection'])
+            algo = item['algo']
+            values = item['constant']['values']
+            description = item['constant']['description']
+            self.constants[algo].append(Constant(values = values, description = description))
--- crylib/db/__init__.py
+++ crylib/db/__init__.py
@@ -8,24 +8,17 @@ Signature Database:
 ↳ {count_algo(dbs)} algorithm
 ↳ {count_constants(dbs)} constants''')
 
-def print_db(title, url):
-    text = f'\n{"-" * 30}\n'
-    text += f'{title}\n'
-    if url: text += f'↳ Refer to: {url}\n'
-    text += f'{"-" * 30}'
-    print(text)
-
 def count_algo(dbs):
     ans = 0
     for db in dbs:
-        ans += len(db.collections.keys())
+        ans += len(db.constants.keys())
     return ans
 
 def count_constants(dbs):
     ans = 0
     for db in dbs:
-        for collections in db.collections.values():
-            ans += len(collections)
+        for constants in db.constants.values():
+            ans += len(constants)
     return ans
 
 dbs = [cryFindDB, cryptoAPIDB, kryptoAnalyzerDB]
