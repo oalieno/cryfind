22a1698434e96124d167e7c95ecbf9ab85b2f176
oalieno <jeffrey6910@gmail.com>
2019-10-22 02:14:25 +0800

    catch KeyboardInterrupt

--- cryfind.py
+++ cryfind.py
@@ -32,5 +32,8 @@ def main():
     search.run(level = level)
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n[-] You Pressed Ctrl-C")
 
