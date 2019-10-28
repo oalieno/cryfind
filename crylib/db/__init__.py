f031d2138fc611d55ce1033b34fce782bafa0f18
oalieno <jeffrey6910@gmail.com>
2019-10-28 17:11:17 +0800

    remove useless functions

--- crylib/db/__init__.py
+++ crylib/db/__init__.py
@@ -3,23 +3,4 @@ from .CryptoAPI import cryptoAPIDB
 from .KryptoAnalyzer import kryptoAnalyzerDB
 from .Polichombr import PolichombrDB
 
-def print_stats(dbs):
-    print(f'''
-Signature Database:
-↳ {count_algo(dbs)} algorithm
-↳ {count_constants(dbs)} constants''')
-
-def count_algo(dbs):
-    ans = 0
-    for db in dbs:
-        ans += len(db.constants.keys())
-    return ans
-
-def count_constants(dbs):
-    ans = 0
-    for db in dbs:
-        for constants in db.constants.values():
-            ans += len(constants)
-    return ans
-
 dbs = [cryFindDB, cryptoAPIDB, kryptoAnalyzerDB, PolichombrDB]
