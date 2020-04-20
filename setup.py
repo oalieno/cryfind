16806d92bbcd3952fdc76a4621604c66bc401852
oalieno <jeffrey6910@gmail.com>
2020-04-20 16:39:11 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='cryfind',
+    version='1.0.0',
+    description='Tool to find cryptographic constants',
+    install_requires=['ahocorapy==1.5.2',
+                      'yara-python==3.10.0',
+                      'docopt==0.6.2',
+                      'lief==0.10.1',
+                      'r2pipe==1.4.2'],
+    author='oalieno',
+    author_email='jeffrey6910@gmail.com',
+    license='MIT License',
+    packages = find_packages(),
+    scripts = ['cryfind', 'crygen']
+)
