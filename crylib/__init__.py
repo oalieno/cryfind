020714111455453649ff075f0061832b2cafd0d4
oalieno <jeffrey6910@gmail.com>
2020-07-22 16:03:09 +0800

    breaking changes, replacing searching core with yara engine

--- crylib/__init__.py
+++ crylib/__init__.py
@@ -1,5 +1,5 @@
-from .findconst import find_const, find_const_yara
+from .findconst import find_const
 from .stackstrings import stackstrings
 from .peimport import pe_import
 
-__all__ = ['find_const', 'find_const_yara', 'stackstrings', 'pe_import']
+__all__ = ['find_const', 'stackstrings', 'pe_import']
--- crylib/findconst.py
+++ crylib/findconst.py
@@ -1,108 +1,6 @@
-import copy
-import string
-from collections import defaultdict
-
 import yara
-from ahocorapy.keywordtree import KeywordTree
-
-class Node:
-    def __init__(self, address, level):
-        self.levels = [level]
-        self.addresses = [address]
-
-def _lmap(f, x):
-    return list(map(f, x))
-
-def _unique(x):
-    return len(set(x)) == len(x)
-
-def _distance(x, y):
-    z = x.addresses + y.addresses
-    return max(z) - min(z)
-
-def _same_level(x, y):
-    return len(set(x.levels) & set(y.levels)) > 0
-
-def _merge(x, y):
-    # ensure the levels is order after merge
-    levels, addresses = [], []
-    i, j = 0, 0
-    while i < len(x.levels) and j < len(y.levels):
-        if x.levels[i] < y.levels[j]:
-            levels.append(x.levels[i])
-            addresses.append(x.addresses[i])
-            i += 1
-        else:
-            levels.append(y.levels[j])
-            addresses.append(y.addresses[j])
-            j += 1
-    while i < len(x.levels):
-        levels.append(x.levels[i])
-        addresses.append(x.addresses[i])
-        i += 1
-    while j < len(y.levels):
-        levels.append(y.levels[j])
-        addresses.append(y.addresses[j])
-        j += 1
-    x.levels = levels
-    x.addresses = addresses
-    return x
-
-def _find_addresses(table, values):
-    lens = _lmap(lambda x: len(table[x]), values)
-    if not _unique(values):
-        return []
-    elif sum(lens) > 100:
-        # if too many nodes, use simple heuristic
-        total = min(lens)
-        groups = [[] for _ in range(total)]
-        for value in values:
-            xs = sorted(table[value])[:total]
-            for i, x in enumerate(xs):
-                groups[i].append(x)
-        return groups
-    else:
-        nodes, total = [], min(lens)
-        for i, value in enumerate(values):
-            for address in table[value]:
-                nodes.append(Node(address, i))
-
-        complete = 0
-        while complete < total:
-            best_d, best_i, best_j = None, None, None
-            for i in range(len(nodes)):
-                for j in range(i + 1, len(nodes)):
-                    if not _same_level(nodes[i], nodes[j]):
-                        d = _distance(nodes[i], nodes[j])
-                        if best_d is None or d < best_d:
-                            best_d, best_i, best_j = d, i, j
-            if best_d is None:
-                break
-
-            if len(nodes[best_i].levels) + len(nodes[best_j].levels) == len(values):
-                complete += 1
-            nodes = nodes[:best_i] + \
-                    nodes[best_i+1:best_j] + \
-                    nodes[best_j+1:] + \
-                    [_merge(nodes[best_i], nodes[best_j])]
-
-        groups = []
-        for node in nodes:
-            if len(node.addresses) == len(values):
-                groups.append(node.addresses)
-        return groups
-
-def _xordiff(data):
-    answer = []
-    for i in range(len(data) - 1):
-        answer.append(data[i] ^ data[i+1])
-    return bytes(answer)
-
-def _two_complement(value, bits, i_byteorder='big', o_byteorder='big'):
-    value = int.from_bytes(value, i_byteorder)
-    if (value & (1 << (bits - 1))) != 0:
-        value = value - (1 << bits)
-    return abs(value).to_bytes(bits // 8, o_byteorder)
+import struct
+from collections import defaultdict, deque
 
 def _cut(x, n):
     ans = []
@@ -110,103 +8,203 @@ def _cut(x, n):
         ans.append(x[i:i+n])
     return ans
 
-def find_const(binarys, constants, encode=False, xor=False):
-    if isinstance(binarys, bytes):
-        binarys = [binarys]
+def _negative(value, c_type):
+    value = struct.unpack(c_type, value)[0]
+    return struct.pack(c_type, -value)
 
-    binarys, binarys_bak = copy.deepcopy(binarys), binarys
+def _id_unique(values):
+    _id, _values = [], []
+    for i, value in enumerate(values):
+        if value not in _values:
+            _id.append(i)
+            _values.append(value)
+    return list(zip(_id, _values))
 
-    constants_new = []
+def _xor(a, b):
+    return bytes([x ^ y for x, y in zip(a, b)])
+
+def _xor_difference(data, size = 1):
+    ans = int.from_bytes(data[:-size], 'big') ^ int.from_bytes(data[size:], 'big')
+    return ans.to_bytes(len(data) - size, 'big')
+
+def _constant_to_rule(constant, size = 'fullword', _ctr = [0]):
+    _ctr[0] += 1
+    size = {
+        'fullword': len(constant['value']),
+        'dword': 4,
+        'qword': 8
+    }[size]
+    
+    values = _cut(constant['value'], size)
+    
+    rules = f'''rule cry_{_ctr[0]} {{
+    meta:
+        name = "{constant['name']}"
+        length = "{len(values)}"
+    strings:
+'''
+   
+    for i, value in enumerate(values):
+        for _id, encoding in _id_unique([
+            value,
+            value[::-1]
+        ] + ([
+            _negative(value, {4: 'i', 8: 'q'}[size])
+        ] if size in [4, 8] else [])):
+            rules += f'        $c_{i}_{_id} = {{ {encoding.hex()} }}\n'
+
+    rules += f'''    condition:
+        {' and '.join([f'(any of ($c_{i}_*))' for i in range(len(values))])}
+}}
+'''
+
+    return rules
+
+def _constants_to_rules(constants, size = 'fullword'):
+    rules = ''
     for constant in constants:
-        v = constant['values']
+        constant = {**constant, 'value': constant['value'][:64]}
+        s = {
+            'fullword': len(constant['value']),
+            'dword': 4,
+            'qword': 8
+        }[size]
+        if len(constant['value']) % s != 0:
+            continue
+        elif any([b'\x00' * 3 in value for value in _cut(constant['value'], s)]):
+            continue
+        rules += _constant_to_rule(constant, size)
 
-        constants_new.append({
-            'name': constant['name'],
-            'values': [v]
-        })
+    return rules
 
-        if encode:
-            if b'\x00' * 3 in v:
-                continue
-            if all(map(lambda x: chr(x) in string.printable, v)):
-                continue
-
-            valueses = []
-            if len(v) % 4 == 0:
-                valueses.append(_cut(v, 4))
-            if len(v) % 8 == 0:
-                valueses.append(_cut(v, 8))
-            for values in valueses:
-                # big, little endian
-                for e in ['big', 'little']:
-                    constants_new.append({
-                        'name': constant['name'],
-                        'values': _lmap(lambda x: x[::-1] if e == 'little' else x, values),
-                        'encode': f'{e} endian',
-                        'size': len(values[0])
-                    })
-                # two's complement
-                for i in ['big', 'little']:
-                    for o in ['big', 'little']:
-                        constants_new.append({
-                            'name': constant['name'],
-                            'values': _lmap(lambda x: _two_complement(x, len(x) * 8, i, o), values),
-                            'encode': f"{i} endian -> two's complement -> {o} endian",
-                            'size': len(values[0])
-                        })
-
-    constants, constants_bak = copy.deepcopy(constants_new), constants_new
-
-    if xor:
-        for constant in constants:
-            constant['values'] = _lmap(_xordiff, constant['values'])
-        binarys = _lmap(_xordiff, binarys)
-
-    kwtree = KeywordTree()
-    for constant in constants:
-        for value in constant['values']:
-            kwtree.add(value)
-    kwtree.finalize()
-
+def _search_yara(binary, constants, size = 'fullword'):
+    rules = _constants_to_rules(constants, size)
     results = []
-    for binary, binary_bak in zip(binarys, binarys_bak):
-        # get table of pattern location -> {b'abcd': [0x01, 0x10], b'efgh': [0x06], ...}
-        table = defaultdict(list)
-        for value, address in kwtree.search_all(binary):
-            table[value].append(address)
-
-        # find addresses of constant -> [[0x01, 0x02, 0x03, 0x04], [0x10, 0x11, 0x12, 0x13]]
-        for constant, constant_bak in zip(constants, constants_bak):
-            groups = _find_addresses(table, constant['values'])
-            for group in groups:
-                result = {
-                    'name': constant['name'],
-                    'address': min(group),
+    for match in yara.compile(source=rules).match(data=binary): 
+        result = {
+            'name': match.meta['name'],
+            'length': int(match.meta['length']),
+            'size': size,
+            'values': [None] * int(match.meta['length'])
+        }
+        for address, varname, value in match.strings:
+            _, index, encoding = varname.split('_')
+            index = int(index)
+            if result['values'][index]:
+                result['values'][index]['addresses'].append(address)
+            else:
+                result['values'][index] = {
+                    'value': value,
+                    'encoding': [
+                        'big',    # big endian
+                        'little', # little endian
+                        'neg'     # negative
+                    ][int(encoding)],
+                    'addresses': [address],
+                    'index': index
                 }
-                if max(group) - min(group) != len(constant['values'][0]) * (len(constant['values']) - 1):
-                    result['address_distribution'] = group
-                xor = binary_bak[group[0]] ^ constant_bak['values'][0][0]
-                if xor > 0:
-                    result['xor'] = xor
-                if constant.get('size'):
-                    result['size'] = constant['size']
-                if constant.get('encode'):
-                    result['encode'] = constant['encode']
-                results.append(result)
-
+        results.append(result)
     return results
 
-def find_const_yara(binarys, rules):
-    if isinstance(binarys, bytes):
-        binarys = [binarys]
+def _search_yara_xor(binary, constants, size = 'fullword', xor_size = 1):
+    _binary = _xor_difference(binary, xor_size)
+    _constants = []
+    for constant in constants:
+        if len(constant['value']) >= xor_size + 4:
+            _constants.append({
+                'name': constant['name'],
+                'value': _xor_difference(constant['value'], xor_size)
+            })
+    _results = _search_yara(_binary, _constants)
+    to_ori = {}
+    for constant in constants:
+        to_ori[constant['name']] = constant['value']
+    results = []
+    for _result in _results:
+        result = {
+            'name': _result['name'],
+            'size': _result['size'],
+            'values': []
+        }
+        for _value in _result['values']:
+            addresses = []
+            # filtering false positive cases
+            for address in _value['addresses']:
+                real = binary[address:address+len(_value['value'])]
+                key = _xor(real, to_ori[_result['name']])
+                key = set(_cut(key, xor_size))
+                if len(key) == 1 and int.from_bytes(list(key)[0], 'big') != 0:
+                    addresses.append(address)
+            if addresses:
+                result['values'].append({
+                    'value': real,
+                    'encoding': _value['encoding'],
+                    'addresses': addresses,
+                    'xor': list(key)[0].hex(),
+                    'index': _value['index']
+                })
+        if result['values']:
+            results.append(result)
+    return results
+
+def _auto_group(result):
+    items = []
+    for value in result['values']:
+        for address in value['addresses']:
+            items.append({
+                **value,
+                'addresses': [address]
+            })
+    items.sort(key = lambda x: x['addresses'][0])
 
     results = []
-    for binary in binarys:
-        for rule in rules:
-            y = yara.compile(source=rule)
-            matches = y.match(data=binary)
-            for match in matches:
-                _, address, meta = match.rule, match.strings[0][0], match.meta
-                results.append({'name': meta['name'], 'address': address})
 
+    # TODO: consider distances of address instead of index?
+    while True:
+        L, H, ans = 1, len(items), None
+        while L < H or (L == H and (not ans or ans[1] != L)):
+            M = (L + H) >> 1
+            slots = [0] * len(result['values'])
+            for i in range(M):
+                slots[items[i]['index']] += 1
+            for i in range(len(items) - M + 1):
+                if all([slot > 0 for slot in slots]):
+                    ans, H = (i, M), M
+                    break
+                if i + M < len(items):
+                    slots[items[i]['index']] -= 1
+                    slots[items[i+M]['index']] += 1
+            else:
+                L = M + 1
+
+        if ans:
+            values, dels, addresses = [None] * len(result['values']), [], []
+            for i in range(ans[0], ans[0] + ans[1]):
+                value = items[i]
+                if not values[value['index']]:
+                    values[value['index']] = value
+                    dels.append(i)
+                    addresses.append(value['addresses'][0])
+            for i in dels[::-1]:
+                del items[i]
+            if max(addresses) - min(addresses) <= 0x1000:
+                results.append({**result, 'values': values})
+        else:
+            break
+        
+    return results
+
+def find_const(binary, constants, auto_group = True):
+    results = []
+    for size in ['fullword', 'dword', 'qword']:
+        results += _search_yara(binary, constants, size)
+    for xor_size in range(1, 3):
+        results += _search_yara_xor(binary, constants, size, xor_size)
+    if not auto_group:
+        _results = []
+        for result in results:
+            grouped_results = _auto_group(result)
+            if grouped_results:
+                _results += grouped_results
+        results = _results
     return results
