83471f017e82035094aaa619309937f21f284c4e
oalieno <jeffrey6910@gmail.com>
2020-08-06 11:43:52 +0800

    add xor test

--- test/test_find_api.py
+++ test/test_find_api.py
@@ -3,4 +3,5 @@ from crylib import find_api
 
 def test_basic():
     results = find_api(b'xxxxxxxxxhelloxxxxxxworldxxxx', {'test.dll': ['hello', 'world']})
-    assert(len(results[0].groups[0].values) == 2)
+    assert(len(results) == 1)
+    assert(len(results[0]['functions']) == 2)
