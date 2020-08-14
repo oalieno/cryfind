cf854277c710e31a8b43ff74167957e0ade8d204
oalieno <jeffrey6910@gmail.com>
2020-08-14 12:13:06 +0800

    add new xor test

--- test/test_find_const.py
+++ test/test_find_const.py
@@ -13,6 +13,10 @@ def test_xor():
     results = find_const(b'gdeb7452301>', [{'name': 'abcd', 'value': b'abcd12345678'}])
     assert(len(results) == 1)
 
+def test_xor_2():
+    results = find_const(b'adfeiv\t\x0b\x0b\rY[[]]__Qwreoij', [{'name': 'abcd', 'value': b'abcd12345678'}])
+    assert(len(results) == 1)
+
 def test_complex():
     results = find_const(bytes.fromhex('44444444000044444444efeeeeeededddddd00000000333333330000'), [{'name': 'Winnti Custom Decoder', 'value': b'\x11\x11\x11\x11\x22\x22\x22\x22\x33\x33\x33\x33\x44\x44\x44\x44'}])
     assert(len(results) == 1)
